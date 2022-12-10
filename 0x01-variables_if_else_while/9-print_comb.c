a6cf2a2ce6e7a6fbf3f6b776e0c7f01111778dde
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2022-12-10 02:13:13 +0200

    Adding 9 print comp

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print possible combination of single digit number
+ *
+ * Return: (0) Sucess
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		if (num == 9)
+			break;
+
+		putchar(',') && putchar(' ');
+	}
+
+	return (0);
+}
