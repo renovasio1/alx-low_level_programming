b8b819f04bd564c376e3d4598465be72a21809ba
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2022-12-15 03:20:52 +0200

    adding 9.multiply

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,23 +7,25 @@
  */
 void times_table(void)
 {
-	int i, j;
-	int n = 9;
+	int i, j, mult;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= n; j++)
-		{
-			int mul =  i * j;
+		_putchar('0');
 
-			if (mul <= 9)
-			{
-				printf(" %d", mul);
-			}
-			else
-					printf("%d", mul);
+		for (j = 1; j <= 9; j++)
+		{
 			_putchar(',');
 			_putchar(' ');
+
+			mult = i * j;
+
+			if (mult <= 9)
+				_putchar(' ');
+			else
+				_putchar((mult / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
