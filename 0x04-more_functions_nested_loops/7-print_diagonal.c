1cb5b200fe953989c7c7969d9b7114cf7842448a
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2022-12-15 18:59:03 +0200

    Adding 7-print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal
+ *
+ *@n: number of char \
+ *
+ * return: diagonal
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
