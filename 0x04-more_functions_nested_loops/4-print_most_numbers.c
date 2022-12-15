5d36aa37c9c72fd78d026a7bb841a804c561a6e8
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2022-12-15 18:06:47 +0200

    Adding 4-print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints nums from 0-9
+ *
+ * Return: nums from 0-9 without 2-4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9 ; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
