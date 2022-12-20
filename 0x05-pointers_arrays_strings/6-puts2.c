724d3c0f53ec6bd9b42abe2c7084ee0feb37d206
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2022-12-20 02:39:28 +0200

    Adding 6- puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints other num
+ *
+ *
+ * @str: array
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n);
+}
