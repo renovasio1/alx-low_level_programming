7e44a0cbf51f9f0dd8ed71adf4c77e6191d08be4
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2023-01-02 22:16:19 +0200

    Adding 2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - search for c in *s
+ *
+ *@s: string to be searched in
+ *@c: letter to search for
+ *
+ *Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
