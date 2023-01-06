1552863d1270c2e7364c979d0e305135efbc1ec7
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2023-01-07 01:59:58 +0200

    Adding 3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int result = 1;
 
+	for (i = 1; i < argc; i++)
+	{
+		result *= atoi(argv[i]);
+	}
 	if (argc < 3)
-		printf("Error");
+		printf("Error\n");
 	else
-		for (i = 1; i < argc; i++)
-		{
-			result *= atoi(argv[i]);
-		}
-	printf("%d\n", result);
+		printf("%d\n", result);
 	return (0);
 }
