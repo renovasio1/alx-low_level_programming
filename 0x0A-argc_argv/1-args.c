0e144c5181aaaa4c33d3cf8436dd2c5aa9fae9cb
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2023-01-07 01:25:09 +0200

    Adding 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: string of arguments
+ * Return: (0) Always sucess
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	(void) argv;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
