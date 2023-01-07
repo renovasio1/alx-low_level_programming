39848bd81ab4ebc240c8c2be3e360a5ce268a34e
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2023-01-07 02:42:11 +0200

    Adding add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: num of arguments
+ * @argv: arguments
+ * Return: 1 if error and 0 if sucess
+ */
+int main(int argc, char *argv[])
+{
+	int total, i;
+	char *p;
+	int num;
+
+	total = 0;
+	if (argc > 1)
+	{
+		for (i = 1; argv[i]; i++)
+		{
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += num;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}
