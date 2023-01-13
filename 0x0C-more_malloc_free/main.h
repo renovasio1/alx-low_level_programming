d7adf48dab36836d84ab2c576b7b3c93d83ae541
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2023-01-13 23:52:42 +0200

    Adding 2-calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+
+void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 void free_grid(int **grid, int height);
