df2bd77a7ebb303591bbf0d518f2e42f8770461b
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2023-01-13 23:28:24 +0200

    Adding 0x0c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+void *malloc_checked(unsigned int b);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+#endif
