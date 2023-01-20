90e7af4e10bb42e5076a4a419af191df8a496e39
Ahmed Elsharkawy <ahmed_nehad99@hotmail.com>
2023-01-20 17:53:24 +0200

    Adding 0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,22 +5,11 @@
  * @name: the name given
  * @f: function of name
  *
- * Return: void
+ * Rteurn: void
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 
 		f(name);
-}#include "function_pointers.h"
-
-/**
- * print_name - Print a name from a function pointer
- * @name: char string
- * @f: function pointer that takes a string argument
- */
-
-void print_name(char *name, void (*f)(char *))
-{
-	f(name);
 }
